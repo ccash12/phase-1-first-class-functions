5f004b765fc1839620b5c1ba712dd435737c93f1
Courtney Cash <cashcourtney97@gmail.com>
2021-08-16 19:32:58 -0400

    commit tree 579c87eb91dd5d4dccaaba508f6c34062b622512

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 function receivesAFunction(callback) {
-    callback ();
+    callback();
 }
 
 function returnsANamedFunction() {
     return function namedFn() {
-        console.log("this is a named function");
-    };
+        console.log('this is a named function ')
+    }
 }
 
 function returnsAnAnonymousFunction() {
     return function() {
-        console.log("this is an anonymous function");
+        console.log('returns an anonymous function')
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function receivesAFunction(callback) {
+//     callback ();
+// }
+
+// function returnsANamedFunction() {
+//     return function namedFn() {
+//         console.log("this is a named function");
+//     };
+// }
+
+// function returnsAnAnonymousFunction() {
+//     return function() {
+//         console.log("this is an anonymous function");
+//     }
+// }
